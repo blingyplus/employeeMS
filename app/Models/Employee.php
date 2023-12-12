cea0aa284b5abde287ff14d0b01cb75081893e88
Russel D. Boakye <113293972+blingyplus@users.noreply.github.com>
2023-12-12 11:16:02 +0000

    set default value of uuid

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,10 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Employee extends Model
 {
     use HasFactory;
 
     protected $fillable = ['uuid', 'firstname', 'lastname', 'phone', 'email'];
+
+    protected static function booted()
+    {
+        static::creating(function ($employee) {
+            $employee->uuid = Str::uuid();
+        });
+    }
 }
